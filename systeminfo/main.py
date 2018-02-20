9688bc0c84b3e97ba56623ba6961a0f27b6a75a9
Shauna Porter <shauna.porter@ucdconnect.ie>
2018-02-20 16:56:07 +0000

    Commit

---
+++ systeminfo/main.py
@@ -0,0 +1,13 @@
+'''
+Created on 25 Jan 2018
+
+@author: Slaporter
+'''
+import platform
+
+def get_platform_info():
+    return (platform.platform())
+     
+
+if __name__ == '__main__':
+    get_platform_info()
